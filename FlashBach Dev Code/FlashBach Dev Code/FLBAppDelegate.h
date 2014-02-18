224d7a4d8c58f94259f000b65691d3f17a0132fd
AAlderman20 <cs121@wl-194-6.wpa.claremont.edu>
2014-02-18 11:37:27 -0800

    Added new project to Git version control.

---
+++ FlashBach Dev Code/FlashBach Dev Code/FLBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FLBAppDelegate.h
+//  FlashBach Dev Code
+//
+//  Created by CS121 on 2/18/14.
+//  Copyright (c) 2014 CS121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlashBach Dev Code/FlashBach Dev Code/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FlashBach Dev Code
+//
+//  Created by CS121 on 2/18/14.
+//  Copyright (c) 2014 CS121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FLBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FLBAppDelegate class]));
+    }
+}
