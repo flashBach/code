f486138b8b1f6ab4851608a926e69894946e8b57
Taylor Peterson <tpeterson@hmc.edu>
2014-03-06 13:38:05 -0800

    Added 'skip' button and button border view controllers.
    Button border view controllers currently not working; cannot figure out
    how to ctrl-drag an outlet into the view controller.

---
+++ FlashBach Dev Code/FlashBach Dev Code/FLBButtonBorderViewController.h
@@ -0,0 +1,15 @@
+//
+//  FLBButtonBorderViewController.h
+//  FlashBach Dev Code
+//
+//  Created by CS121 on 3/6/14.
+//  Copyright (c) 2014 CS121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FLBButtonBorderViewController : UIViewController
+
+
+
+@end
